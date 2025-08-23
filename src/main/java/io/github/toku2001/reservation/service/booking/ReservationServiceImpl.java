942c9c9902b31a8204f8fccaf6dc690fc18c4747
NakamuraTokuhisa <pole0500tngwz@gmail.com>
2025-08-23 21:47:18 +0900

    更新機能の追加・キャッシュに保存するキーをidからuserIdに変更(idは一意キーの為、多くのアクセスが同じidを検索条件にくることはないと判断)

--- src/main/java/io/github/toku2001/reservation/service/booking/ReservationServiceImpl.java
+++ src/main/java/io/github/toku2001/reservation/service/booking/ReservationServiceImpl.java
@@ -1,8 +1,8 @@
 package io.github.toku2001.reservation.service.booking;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -14,10 +14,13 @@ public class ReservationServiceImpl implements ReservationService {
 	
 	@Autowired
 	private ReservationMapper reservationMapper;
+	
+	@Autowired
+	private CacheManager cacheManager;
 
 	@Override
-	@CachePut(value = "reservationCache", key = "#reservation.id")
 	public boolean createBooking(Reservation reservation) {
+		System.out.println(">>> DBアクセス発生: createBooking(" + reservation.getId() + ")");
 		int resultNumber = reservationMapper.insert(reservation);
 		if(resultNumber == 1) {
 			return true;
@@ -26,18 +29,33 @@ public class ReservationServiceImpl implements ReservationService {
 	}
 	
 	@Override
-	@Cacheable(value = "reservationCache", key = "#id")
-	public Reservation getReservation(Long id) {
-		System.out.println(">>> DBアクセス発生: getReservation(" + id + ")");
-		Reservation reservation = reservationMapper.findById(id);
+	@Cacheable(value = "reservationCache", key = "#userId")
+	public Reservation getReservation(int userId) {
+		System.out.println(">>> DBアクセス発生: getReservation(" + userId + ")");
+		Reservation reservation = reservationMapper.findById(userId);
 		return reservation;
 	}
 	
 	@Override
-	@CacheEvict(value = "reservationCache", key = "#id")
-	public boolean deleteReservation(Long id) {
-		int resultNumber = reservationMapper.deleteById(id);
+	@CacheEvict(value = "reservationCache", key = "#userId")
+	public boolean deleteReservation(int id, int userId) {
+		System.out.println(">>> DBアクセス発生: deleteReservation(" + id + ")(" + userId + ")");
+		int resultNumber = reservationMapper.deleteById(id, userId);
 		if(resultNumber == 1) {
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public boolean updateReservation(Reservation reservation) {
+		System.out.println(">>> DBアクセス発生: updateReservation(" + reservation.getId() + ")");
+		int resultNumber = reservationMapper.updateReservation(reservation);
+		if(resultNumber == 1) {
+			//MyBatisではアップデート文の戻り値を更新行にすることはできないため改め更新行を取得
+			System.out.println(">>> DBアクセス発生: getReservation(" + reservation.getId() + ")");
+			Reservation updated = reservationMapper.findById(reservation.getId());
+			cacheManager.getCache("reservationCache").put(updated.getUserId(), updated);
 			return true;
 		}
 		return false;
